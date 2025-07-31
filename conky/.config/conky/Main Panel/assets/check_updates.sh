8918921fc4b686854b6eb4d60eaa63c2ea4f11d4
Cinar Ozkan <me@cinarozkan.com>
2025-07-31 11:26:07 +0300

    Fixed 'Main Panel' conky configs broken update checker and upscaled 3 wallpapers for better quality

--- conky/.config/conky/Main Panel/assets/check_updates.sh
+++ conky/.config/conky/Main Panel/assets/check_updates.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-export PATH=/usr/bin:/bin:/usr/local/bin
 
-CHECKUPDATES="/usr/bin/checkupdates"
-YAY="/usr/bin/yay"
-PARU="/usr/bin/paru"
+# Get the update count (all the repos and aur)
+updates=$(yay -Qu | wc -l)
 
-sys_updates=$($CHECKUPDATES 2>/dev/null | wc -l)
-
-aur_updates=0
-if [ -x "$YAY" ]; then
-    aur_updates=$($YAY -Qua 2>/dev/null | wc -l)
-elif [ -x "$PARU" ]; then
-    aur_updates=$($PARU -Qua 2>/dev/null | wc -l)
-fi
-
-total_updates=$((sys_updates + aur_updates))
-
-if [ "$total_updates" -eq 0 ]; then
-    echo "System is up-to-date"
+# Return string
+if [ "$updates" -eq 0 ]; then
+  echo "System is up to date"
 else
-    echo "$total_updates updates available"
+  echo "$updates updates available"
 fi
